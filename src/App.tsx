2f01c43e86f2f030604d9c91e4552e059abfe638
BuTomer <bu.tomer@gmail.com>
2023-02-09 11:43:52 +0200

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import { useContext } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import DarkModeContext from "./context/dark-mode-context";
+import Navabar from "./components/Navabar";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+import Register from "./routes/Register";
 
 function App() {
-  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
-
-  return <div className="App">
-    <p>
-      {darkMode ? "DARK" : "NOT DARK"}
-    </p>
-
-    <button onClick={()=>{
-      toggleDarkMode()
-    }}>TOGGLE DARK</button>
-  </div>;
+  return (
+    <>
+      <Navabar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
